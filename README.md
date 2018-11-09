8be1644c92921757e490322874d15b6866036c06
Kevin Thorngren <kthorngr@cisco.com>
2018-11-09 09:47:24 -0500

    Basic babel and webpack setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Node.js with Datatables demo
+
+I am using this project to learn node.js and how to build a development environment and bundle into a deployable package.  This package should have the following:
+
+- Node JS is the server
+- Datatables and Editor for CRUD operations
+- JS based API for reading and writing to the DB
+- Use of JSON Server for test data
+- Sqlite and MySql DBs for production data
+- Bootstrap for styling
---
+++ webpack.config.dev.js
@@ -0,0 +1,34 @@
+import path from 'path';
+
+const config = {
+  //debug: true,
+  devtool: 'inline-source-map',
+  //noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index.js')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    rules : [
+      // JavaScript/JSX Files
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      // CSS Files
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]  }
+}
+
+// Exports
+module.exports = config;
